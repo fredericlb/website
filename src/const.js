065321cbb171bf06fbb6e56e03053d69ca0d3df9
Louis-Rémi Babé <lrbabe@gmail.com>
2018-10-30 17:41:42 +0100

    Update const.js

--- src/const.js
+++ src/const.js
@@ -9,7 +9,7 @@ export default Object.assign({}, common, {
       return 'https://ucyldwzph9.execute-api.eu-west-1.amazonaws.com/staging/forest';
     default:
     case 'development':
-      return 'https://ucyldwzph9.execute-api.eu-west-1.amazonaws.com/staging/forest';
+      return 'http://localhost:3000/forest';
     }
   })(process.env.PREACT_APP_ENV),
 
