b8fdc80eb01da6c95abda8e11a5c4caecc86dc0b
Louis-Rémi Babé <lrbabe@gmail.com>
2018-11-12 15:34:11 +0100

    Modify business logic that displays a warning when the arrival date and availability date are more than 3 days appart

--- src/components/Availability.js
+++ src/components/Availability.js
@@ -4,20 +4,26 @@ function Availability({ lang, className, availableAt, arrivalDate }) {
   let icon = <span className="material-icons">check_circle</span>;
   let text = <Text id="availableNow">Available now</Text>;
   let _class = `${className} availableNow`;
+  const diffInDays =
+    Math.abs((+(arrivalDate || 0)) - Math.max(+availableAt, +new Date())) /
+    (1000 * 60 * 60 * 24);
 
   if ( availableAt === null ) {
     icon = <span className="material-icons">remove_circle</span>;
     text = <Text id="unavailable">Unavailable</Text>;
     _class = `${className} unavailable`;
   }
-  else if ( +availableAt > +Date.now() ) {
-    let date = availableAt.toLocaleDateString().replace(/\/\d{4}/, '');
-    text = (
-      <Text id="availableFrom" fields={{ date }}>
-        {`Available from ${date}`}
-      </Text>
-    );
-    if ( arrivalDate != null && (+availableAt > +arrivalDate) ) {
+  else {
+    if ( +availableAt > +Date.now() ) {
+      let date = availableAt.toLocaleDateString().replace(/\/\d{4}/, '');
+      text = (
+        <Text id="availableFrom" fields={{ date }}>
+          {`Available from ${date}`}
+        </Text>
+      );
+    }
+
+    if ( arrivalDate != null && diffInDays > 3 ) {
       icon = <span className="material-icons">warning</span>;
       _class = `${className} availableFrom`;
     }
