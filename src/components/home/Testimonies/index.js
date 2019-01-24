89a9b8c75300f57b24ee77c456bef8505934c85a
Julien Kilo <julienkilo@gmail.com>
2019-01-24 17:17:20 +0100

    update home page customer reviews

--- src/components/home/Testimonies/index.js
+++ src/components/home/Testimonies/index.js
@@ -5,6 +5,8 @@ function Testimonies({ lang }) {
   return (
     <section className={style.testimonies}>
       {definition[lang].map((t, i) => <Testimony {...t} i={i} />)}
+      {/* Dirty hack to prevent the last item from being displayed out of the section  */}
+      <div style="clear: both" />
     </section>
   );
 }
@@ -24,7 +26,7 @@ function Testimony({ title, comment, author, i }) {
       <p>
         {comment}
       </p>
-      <div className={`${style.authors} text-bold`}>
+      <div className="text-bold">
         {author}
       </div>
     </div>
@@ -33,78 +35,99 @@ function Testimony({ title, comment, author, i }) {
 
 const definition = {
   'en-US': [{
-    title: 'Perfect customer service',
+    title: 'All happiness!',
     comment: `
-      The team was very helpful and friendly throughout my stay.
-      I settled in quickly and easily.
-      The maintenance of the house was impeccable. Thanks :)
+      1 year and a half at Chez Nestor, 1 year and a half of happiness!
+      The team is super responsive and caring. I'm staying on a great
+      experience. :) I recommend it!
     `,
-    author: 'Anna, 22 years old, Australian',
+    author: 'Othmane, Employee, 23 years old, Moroccan',
   }, {
-    title: 'Very cosy flat',
+    title: 'A quality customer service',
     comment: `
-      I really enjoyed discovering the city with my roommates and
-      concentrating on my studies: everything is already ready in the
-      accommodation, there is nothing more to take care of.
+      The accommodation is well equipped and customer service is always
+      available. We receive prompt and complete answers.
     `,
-    author: 'Max, 25 years old, German',
+    author: 'Paola, Student, 23 years old, Brazilian',
   }, {
-    title: 'So simple!',
+    title: 'Well located apartments',
     comment: `
-      The reservation is super simple, and I loved having international
-      roommates. I have already tested the service in 3 cities for 2 years
-      for internships and semesters.
+      I managed to find an apartment right next to my school on the Chez
+      Nestor site.
     `,
-    author: 'Emilie, 23 years old, French',
+    author: 'Mathieu, Student, 29 years old, French',
+  }, {
+    title: 'Easy to find accommodation',
+    comment: `
+      I highly recommend Chez Nestor because they found me a stress-free
+      and easy accommodation. My room was clean, comfortable and had everything
+      I needed. The Chez Nestor team has always been attentive, helpful and
+      understanding.
+    `,
+    author: 'Hannah, Employee, 22 years old, Australian',
   }],
   'fr-FR': [{
-    title: 'Service parfait',
+    title: 'Que du bonheur !',
     comment: `
-      L'équipe a été très utile et agréable pendant tout mon séjour. Mon
-      installation a été facile et rapide. L'entretien du logement était
-      impeccable. Merci :)
+      1 an et demi Chez Nestor, 1 an et demi de bonheur ! L'équipe est super
+      réactive et aux petits soins. Je reste sur une belle expérience. :)
+      Je recommande !
     `,
-    author: 'Anna, 22 ans, Australienne',
+    author: 'Othmane, Salarié, 23 ans, Marocain',
   }, {
-    title: 'Appartement très confortable',
+    title: 'Un service client de qualité',
     comment: `
-      J'ai beaucoup apprécié de découvrir la ville avec mes colocs et me
-      concentrer sur mes études : tout est déjà prêt dans le logement, il
-      n'y a plus rien à s'occuper.
+      Le logement est bien équipé et le service client est toujours disponible.
+      On reçoit des réponses rapides et complètes.
     `,
-    author: 'Max, 25 ans, Allemand',
+    author: 'Paola, Étudiante, 23 ans, Brésilienne',
   }, {
-    title: 'Super simple !',
+    title: 'Des appartements bien situés',
     comment: `
-      La réservation est super simple, et j'ai adoré avoir des colocataires
-      internationaux. J'ai déjà testé le service dans 3 villes depuis 2 ans
-      pour des stages et semestres.
+      J’ai réussi à trouver un appartement juste à côté de mon école sur le site
+      de Chez Nestor.
     `,
-    author: 'Émilie, 23 ans, Française',
+    author: 'Mathieu, Étudiant, 29 ans, Français',
+  }, {
+    title: 'Facile de trouver son logement',
+    comment: `
+      Je recommande vivement Chez Nestor car ils m’ont trouvé un hébergement sans
+      stress et facilement. Ma chambre était propre, confortable et avait tout ce
+      dont j'avais besoin. L'équipe de Chez Nestor a toujours été attentive,
+      serviable et compréhensive.
+    `,
+    author: 'Hannah, Salariée, 22 ans, Australienne',
   }],
   'es-ES': [{
-    title: 'Servicio perfecto',
+    title: '¡ Toda la felicidad !',
     comment: `
-      El equipo ha sido muy eficaz y agradable a lo largo de mi estancia.
-      Mi mudanza ha sido rápida y sencilla,
-      y el mantenimiento de la casa era impecable. Gracias :)
+      ¡ 1 año y medio en Chez Nestor, 1 año y medio de felicidad ! El equipo es muy
+      receptivo y atento. Me estoy quedando en una gran experiencia. :)
+      ¡ Lo recomiendo !
     `,
-    author: 'Anna, 22 años, Australiana',
+    author: 'Othmane, Empleado, 23 años, Marroquí',
   }, {
-    title: 'Apartamento muy confortable',
+    title: 'Un servicio al cliente de calidad',
     comment: `
-      Me encantó descubrir la ciudad con mis compañeros de cuarto y
-      concentrarme en mis estudios: todo está ya preinstalado en el alojamiento. Un verdadero placer.
+      El alojamiento está bien equipado y el servicio de atención al cliente está
+      siempre disponible. Recibimos respuestas rápidas y completas.
     `,
-    author: 'Max, 25 años, Alemán',
+    author: 'Paola, Estudiante, 23 años, Brasileña',
   }, {
-    title: 'Muy sencillo !',
+    title: 'Apartamentos bien ubicados',
     comment: `
-      El proceso de reserva es sencillo, y me encantó tener compañeros de cuarto
-      internacionales. Ya he probado el servicio en 3 diferentes ciudades durante 2 años
-      para pasantías y semestres universitarios.
+      Conseguí encontrar un apartamento justo al lado de mi escuela en el sitio
+      de Chez Nestor.
     `,
-    author: 'Émilie, 23 años, francesa',
+    author: 'Mathieu, Estudiante, 29 años, Francés',
+  }, {
+    title: 'Alojamiento fácil de encontrar',
+    comment: `
+      Recomiendo encarecidamente a Chez Nestor porque me han encontrado un alojamiento
+      fácil y sin estrés. Mi cuarto estaba limpio, cómodo y tenía todo lo que necesitaba.
+      El equipo de Chez Nestor siempre ha sido atento, servicial y comprensivo.
+    `,
+    author: 'Hannah, Empleada, 22 años, Australiana',
   }],
 };
 
